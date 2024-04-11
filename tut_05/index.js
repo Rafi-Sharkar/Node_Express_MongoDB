be5baf7c9b5432a91b9f1bab83dacd6a8f1ca63f
Rafi Sharkar <rafisharkar144@gmail.com>
2024-04-11 23:08:57 +0600

    os & path

---
+++ tut_05/index.js
@@ -0,0 +1,21 @@
+// os, path
+//os____
+const os = require("os")
+// console.log(os.userInfo())
+// console.log(os.homedir())
+// console.log(os.hostname())
+// console.log(os.totalmem())
+console.log(os.freemem())
+
+//Path____
+// console.log(__dirname)
+// console.log(__filename)
+
+const path = require("path")
+
+const extensionName = path.extname("index.html")
+
+console.log(extensionName)
+
+const joinName = path.join(__dirname + "/../views")
+console.log(joinName)
